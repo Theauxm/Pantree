9d7053ca24328ed647c702ff3de3ebc90ce2e95f
Theauxm <theaux.masquelier@gmail.com>
2021-03-31 14:21:02 -0600

    Deleted un-necessary files

--- webscraping/recipe_scraping.py
+++ webscraping/recipe_scraping.py
@@ -1,25 +1,27 @@
 from recipe_scrapers import scrape_me
 
+# Sets up website to scrape
 top_level_website = scrape_me('https://allrecipes.com')
+data = open("./data.txt", "w")
 
+# Uses a set to guarantee no duplicates
 ingredients = {}
 
+# Gets all recipe subsections
 for i in top_level_website.links():
     if 'https://www.allrecipes.com/recipes' not in i['href']:
         continue
 
+    # For each subsection, gets all recipes
     scraper = scrape_me(i['href'])
     for j in scraper.links():
         if 'https://www.allrecipes.com/recipe/' not in j['href']:
             continue
 
+        # Adds recipe to file
         recipe = scrape_me(j['href'])
         if recipe.title() not in ingredients:
             ingredients[recipe.title()] = recipe.ingredients()
+            data.write("TITLE: " + str(recipe.title()) + " INGREDIENTS: " + str(recipe.ingredients()) + "\n")
 
-            print(len(ingredients))
-
-        
-
-
-print(ingredients)
+data.close()
