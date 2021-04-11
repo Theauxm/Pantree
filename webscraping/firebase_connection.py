db47d944befad6c13320e82584e6f4866c5d8b77
Theauxm <theaux.masquelier@gmail.com>
2021-04-10 22:14:55 -0600

    Can successfully write to database

--- webscraping/firebase_connection.py
+++ webscraping/firebase_connection.py
@@ -1,6 +1,12 @@
 import firebase_admin
+from firebase_admin import firestore
 
-
-cred = firebase_admin.credentials.Certificate('../../CS4000/pantree-4347e-firebase-adminsdk-4neb9-82918d559e.json')
+cred = firebase_admin.credentials.Certificate('./webscraping/database_admin_key.json')
 default_app = firebase_admin.initialize_app(cred)
 
+db = firestore.client()
+
+doc_ref = db.collection(u'users').document(u'Obama')
+doc_ref.set({
+    u'name: ' : 'Barack Obama'
+})
