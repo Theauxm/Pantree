7f4fcabbd15ec210d2b3b08de3bdaa52ec68c728
Theauxm <theaux.masquelier@gmail.com>
2021-04-12 16:18:11 -0600

    Recipes adding, however duplicates are being created

--- webscraping/firebase_connection.py
+++ webscraping/firebase_connection.py
@@ -10,3 +10,21 @@ doc_ref = db.collection(u'users').document(u'Obama')
 doc_ref.set({
     u'name: ' : 'Barack Obama'
 })
+
+
+ref = db.collection(u'food').stream()
+for doc in ref:
+    print(f'{doc.id} => {doc.to_dict()}')
+
+ref_2 = db.collection(u'food').document(u'Apple').get()
+ref_3 = db.collection(u'food').document(u'Cocaine').get()
+
+if ref_2.exists:
+    print(f'Document data: {ref_2.to_dict()}')
+else:
+    print("Didn't get data for 2")
+
+if ref_3.exists:
+    print(f'Document data: {ref_3.to_dict()}')
+else:
+    print("Didn't get data for 2")
