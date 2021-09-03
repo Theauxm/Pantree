372978014b128f9302f34cf9487545248f34d6dc
Trey Lavery <treyjlavery@gmail.com>
2021-09-03 16:11:56 -0600

    Added pantreeUser.dart That hold the PantreeUser Class! Also updated Home/Homescreen/pantry to be using a PantreeUser!!

---
+++ lib/pantreeUser.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class PantreeUser{
+  var name;
+  var email;
+  //(the token we need for our database)
+  var uid;
+  //**Stuff from database
+  var friends;
+  var pantries;
+  var recipes;
+  var shoppingLists;
+
+
+  PantreeUser() {
+    User u = FirebaseAuth.instance.currentUser;
+
+    this.uid = u.uid;
+    this.name = u.displayName;
+    this.email = u.email;
+
+    FirebaseFirestore.instance.collection('users').doc(u.uid).get()
+        .then((DocumentSnapshot documentSnapshot) => {
+      if (documentSnapshot.exists) {
+        print('Document is Real and added info to the User baby'),
+        this.shoppingLists = documentSnapshot['Shopping IDs'],
+        this.friends = documentSnapshot['Friend IDs'],
+        this.recipes = documentSnapshot['Recipe IDs'],
+        this.pantries = documentSnapshot['Pantry IDs'],
+      }
+    });
+  }
+}
+
+PantreeUser createPantreeUser() {
+  PantreeUser user = PantreeUser();
+  User u = FirebaseAuth.instance.currentUser;
+
+  user.uid = u.uid;
+  user.name = u.displayName;
+  user.email = u.email;
+
+  FirebaseFirestore.instance.collection('users').doc(u.uid).get()
+      .then((DocumentSnapshot documentSnapshot) => {
+        if (documentSnapshot.exists) {
+            print('Document is Real and added info to the User baby'),
+            user.shoppingLists = documentSnapshot['Shopping IDs'],
+            user.friends = documentSnapshot['Friend IDs'],
+            user.recipes = documentSnapshot['Recipe IDs'],
+            user.pantries = documentSnapshot['Pantry IDs'],
+          }
+      });
+  return user;
+}
