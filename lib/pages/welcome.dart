1d78f6577f2e97b87ad0cf62ef48336ab8f5eb93
Trey Lavery <treyjlavery@gmail.com>
2021-09-04 19:36:23 -0600

    Added Signing in on Welcomepage. Still cant create new users just yet!

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -7,92 +7,173 @@ import 'package:firebase_auth_ui/providers.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
+
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Welcome!"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset('assets/images/prototype_logo.png'),
-            Text('Email address'),
-            TextField(decoration: InputDecoration(
-              labelText:"Email"
-            )),
-            Text('Password'),
-            TextField(decoration: InputDecoration(
-                labelText:"password"
-            )),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to first route when tapped.
-                try {
-                  FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: "treyjlavery@gmail.com",
-                      password: "password"
-                  );
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'user-not-found') {
-                    print('No user found for that email.');
-                  } else if (e.code == 'wrong-password') {
-                    print('Wrong password provided for that user.');
-                  }
-                }
-                // FirebaseAuthUi.instance()
-                //     .launchAuth(
-                //   [
-                //     AuthProvider.email(), // Login/Sign up with Email and password
-                //     // AuthProvider.google(), // Login with Google
-                //     // AuthProvider.facebook(), // Login with Facebook
-                //     // AuthProvider.twitter(), // Login with Twitter
-                //     // AuthProvider.phone(), // Login with Phone number
-                //   ],
-                //   // tosUrl: "https://my-terms-url", // Optional
-                //   // privacyPolicyUrl: "https://my-privacy-policy", // Optional,
-                // )
-                //     .then((firebaseUser) =>
-                // //print("Logged in user is ${firebaseUser.displayName}"))
-                // handleNewUsers(firebaseUser.uid, firebaseUser.displayName)
-                //     .catchError((error) => print("Error $error")));
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home(title: 'Pantree Home')));
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.amber[800],
-                onPrimary: Colors.white,
+  _WelcomePage createState() => _WelcomePage();
+}
+
+class _WelcomePage extends State<WelcomePage> {
+
+  TextEditingController textControllerEmail = TextEditingController();
+  TextEditingController textControllerPassword = TextEditingController();
+  String _loginError = "gg";
+
+  void signIn() {
+    try {
+      FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: textControllerEmail.text,
+          password: textControllerPassword.text
+        // email: "treyjlavery@gmail.com",
+        // password: "password"
+      );
+    } catch (e) {
+      if (e.code == 'firebase_auth/user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'firebase_auth/wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text("Welcome!"),
+          ),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                  children: [
+                    Image.asset('assets/images/prototype_logo.png'),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextField(
+                                controller: textControllerEmail,
+                                decoration: InputDecoration(
+                                  labelText: "Email",
+                                  border: OutlineInputBorder(
+                                  ),)),
+                            SizedBox(height: 10),
+                            TextField(
+                                controller: textControllerPassword,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  labelText: "Password",
+                                  border: OutlineInputBorder(
+                                  ),)),
+                            SizedBox(height: 10),
+                            Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Flexible(
+                                  flex: 1,
+                                  child: Container(
+                                    width: double.maxFinite,
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                          backgroundColor: Colors.blue),
+                                      onPressed: () {
+                                        signIn();
+                                      },
+                                      child: Text(
+                                        'Log in',
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.black),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                Flexible(
+                                  flex: 1,
+                                  child: Container(
+                                    width: double.maxFinite,
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                          backgroundColor: Colors.blue),
+                                      onPressed: () {},
+                                      child: Text(
+                                        'Sign up',
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.black),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            //Center(child: GoogleButton()),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // TextField(decoration: InputDecoration(
+                    //   labelText:"Email"
+                    // )),
+                    // Text('Password'),
+                    // TextField(decoration: InputDecoration(
+                    //     labelText:"password"
+                    // )),
+                  ]
               ),
-              child: Text('Sign in'),
-          ),]
-        ),
-      ),
-    );
+            ),
+          ));
+    }
+
+// onPressed: () {
+// Navigator.pop(context);
+// }
   }
 
-  // onPressed: () {
-  // Navigator.pop(context);
-  // }
-}
 
-Future<void> handleNewUsers(String docID, String displayName) async {
-  try {
-    // Get reference to Firestore collection
-    //var collectionRef = FirebaseFirestore.instance.collection('users');
+  Future<void> handleNewUsers(String docID, String displayName) async {
+    try {
+      // Get reference to Firestore collection
+      //var collectionRef = FirebaseFirestore.instance.collection('users');
 
-    //var doc = await collectionRef.doc(docID).get();
-    await FirebaseFirestore.instance.collection('users').doc(docID).get().then((doc) {
-      if (!doc.exists)
-        FirebaseFirestore.instance.collection('users').doc(docID).set({
-          'Username': displayName,
-          'Pantry IDs': [null],
-          'Friend IDs': [null],
-          'Recipe IDs': [null],
-          'Shopping IDs': [null],
-        });
-    });
-  } catch (e) {
-    throw e;
+      //var doc = await collectionRef.doc(docID).get();
+      await FirebaseFirestore.instance.collection('users').doc(docID)
+          .get()
+          .then((doc) {
+        if (!doc.exists)
+          FirebaseFirestore.instance.collection('users').doc(docID).set({
+            'Username': displayName,
+            'Pantry IDs': [null],
+            'Friend IDs': [null],
+            'Recipe IDs': [null],
+            'Shopping IDs': [null],
+          });
+      });
+    } catch (e) {
+      throw e;
+    }
   }
-}
+
+/*
+                FirebaseAuthUi.instance()
+                    .launchAuth(
+                  [
+                    AuthProvider.email(), // Login/Sign up with Email and password
+                    // AuthProvider.google(), // Login with Google
+                    // AuthProvider.facebook(), // Login with Facebook
+                    // AuthProvider.twitter(), // Login with Twitter
+                    // AuthProvider.phone(), // Login with Phone number
+                  ],
+                  // tosUrl: "https://my-terms-url", // Optional
+                  // privacyPolicyUrl: "https://my-privacy-policy", // Optional,
+                )
+                    .then((firebaseUser) =>
+                //print("Logged in user is ${firebaseUser.displayName}"))
+                handleNewUsers(firebaseUser.uid, firebaseUser.displayName)
+                    .catchError((error) => print("Error $error")));
+ */
