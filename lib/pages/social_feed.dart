979f920011a23b24a89510281828fb28ef32c64e
Brandon Wong <brandonw@passivelogic.com>
2021-09-06 13:52:55 -0600

    Finished header for social view, began work on body.

--- lib/pages/social_feed.dart
+++ lib/pages/social_feed.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pantree/pantreeUser.dart';
 
+
 class social_feed extends StatefulWidget {
   PantreeUser user;
   social_feed({this.user});
@@ -12,11 +13,85 @@ class social_feed extends StatefulWidget {
 class _socialState extends State<social_feed> {
   PantreeUser user;
   _socialState({this.user});
-  int _count = 0;
 
+  // @override
+  // void initState(){
+  //   super.initState();
+  //   var uName = user.name;
+  //   print(uName);
+  // }
+
+  // void initialize() async {
+  //   var ll = user.name;
+  // }
+
+  var stars = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.green[500]),
+      const Icon(Icons.star, color: Colors.black),
+      const Icon(Icons.star, color: Colors.black),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('You have pressed the button $_count times.'));
+    //return Center(child: Text('You have pressed the button $_count times.'));
+    //super.build(context);
+    //var user = user.name;
+    //var uName = user.name;
+    return Scaffold(
+      appBar: AppBar(
+
+        leading:IconButton(
+          icon: const Icon(Icons.arrow_left),
+        ),
+
+        //leading: stars,
+
+        title: Text(user.name),
+
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_box_rounded),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('This is a snackbar')));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.view_headline_rounded),
+            tooltip: 'Go to the next page',
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Next page'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'This is the next page',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
+      ),
+
+      body:CircleAvatar(
+        //backgroundImage: NetworkImage(userAvatarUrl),
+        backgroundColor: Colors.green,
+        child: const Text('BW'),
+      )
+    );
+
   }
 }
