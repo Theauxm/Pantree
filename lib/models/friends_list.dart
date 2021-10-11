d57261b0fc1eec658285a571e8fa03d157a637d0
Trey Lavery <treyjlavery@gmail.com>
2021-10-11 17:29:09 -0600

    Added Friends List model

---
+++ lib/models/friends_list.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pantree/pantreeUser.dart';
+
+class FriendsList extends StatefulWidget {
+  final PantreeUser user;
+  FriendsList({this.user});
+
+  @override
+  FriendsListState createState() => FriendsListState(user: user);
+}
+
+// https://stackoverflow.com/questions/51607440/horizontally-scrollable-cards-with-snap-effect-in-flutter
+class FriendsListState extends State<FriendsList> {
+  final PantreeUser user;
+  FriendsListState({this.user});
+
+  Map<String, DocumentReference> friendsMap;// Friends Map, With their names and Doc Ref!
+
+  Future<dynamic> getFriends() async {
+    //await user.updateData(); // important: refreshes the user's data// instantiate the map
+    for (DocumentReference ref in user.friends) {
+      // go through each doc ref and add to list of pantry names + map
+      String friendUsername = "";
+      await ref.get().then((DocumentSnapshot snapshot) {
+        friendUsername = snapshot.data()['Username']; // get the pantry name as a string
+      });
+      friendsMap[friendUsername] = ref; // map the doc ref to its name
+    }
+
+    // very important: se setState() to force a call to build()
+    setState(() {
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getFriends();
+    //setListener();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(friendsMap.toString());
+  }
+}
