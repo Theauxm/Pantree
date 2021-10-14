32bfa7fc502d3f1defe53e071832f3803be4df02
Brandon Wong <brandonw@passivelogic.com>
2021-10-13 18:35:44 -0600

    New Modular ImageTile created.

--- lib/models/ImageTile.dart
+++ lib/models/ImageTile.dart
@@ -1,48 +1,71 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:pantree/pantreeUser.dart';
 
 class ImageTile extends StatefulWidget {
-  PantreeUser user;
-  ImageTile({this.user});
+
+  DocumentReference postRef;
+
+  ImageTile({this.postRef});
 
   @override
-  _ImageTileState createState() => _ImageTileState(user: user);
-}
+  ImageTileState createState() => ImageTileState(postRef: postRef);
 
-class _ImageTileState extends State<ImageTile> {
-  PantreeUser user;
-  _ImageTileState({this.user});
+}
+class ImageTileState extends State<ImageTile>{
+
+  DocumentReference postRef;
+
+  var URL;
+
+  ImageTileState({this.postRef});
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  Future<void> getData() async{
+
+    var post = await postRef.get();
+    var imageDownloadURL;
+    imageDownloadURL = post.data()['image'];
+
+    String imageURL = await firebase_storage.FirebaseStorage.instance
+        .refFromURL(imageDownloadURL)
+        .getDownloadURL();
+
+    this.URL = imageURL;
+    setState(() {
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if(URL == null){
+      return Center(child: CircularProgressIndicator());
+    }
     return
-      Container(
-          padding: const EdgeInsets.all(8),
-          child: GestureDetector(
-            onTap: () async {
-             // getData(); //this onTap needs to be changed
-              },
-            child: Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                  color: Colors.grey[200]),
-              child:
-                //images2.length >= 1
-              //?
-             //   Image.network(
-               //   images2[0])
-           // :
-                Container(
-          decoration: BoxDecoration(
-          color: Colors.teal[100]),
-              width: 200,
-              height: 200,
-            ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: GestureDetector(
+                onTap: () async {
+                  // getData(); //this will be changed to show the larger image preview
+                },
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200]),
+                  child:
+                     Image.network(
+                     this.URL)
+
+                )
             )
-          )
         );
-  }
+      }
 
 }
